55cb6bfd40157a6119757190716edb91d9a13382
Matis Grinbaum <matis@MacBook-Pro-de-Matis.local>
2020-06-26 12:25:48 +0200

    https secure

--- src/containers/Character.js
+++ src/containers/Character.js
@@ -18,7 +18,7 @@ const Character = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`//swapi.dev/api/people/${id}`);
+        const response = await axios.get(`https://swapi.dev/api/people/${id}`);
         setData(response.data);
         setIsLoading(false);
       } catch (error) {
